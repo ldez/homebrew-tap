d5be344cfc7927952eaf1eade3f6a0e5a97e5c66
Fernandez Ludovic <lfernandez.dev@gmail.com>
2019-01-06 21:22:22 +0100

    fix: tar hash.

--- Formula/prm.rb
+++ Formula/prm.rb
@@ -3,7 +3,7 @@ class Prm < Formula
   homepage "https://github.com/ldez/prm"
   url "https://github.com/ldez/prm/releases/download/v2.4.0/prm_v2.4.0_darwin_amd64.tar.gz"
   version "2.4.0"
-  sha256 "762987e8a2e8e3a3f7421f944ca368c349e1525000ef7b40c10cebfa7b7bd8b4"
+  sha256 "b1201cf4c7ee351595eae2f669aed0fe0ad0e775bbb6fa8955a08c1cdfddf332"
 
   def install
     bin.install "prm"
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # homebrew-tap 
 
+[Homebrew Taps](https://docs.brew.sh/Taps)
+
 Homebrew Formulae to [PRM](https://github.com/ldez/prm) binaries, powered by @goreleaser
 
 ```sh
