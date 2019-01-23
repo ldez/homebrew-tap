de2d52d526d173e624241b78815c7a7605132f2b
ldez <ldez@users.noreply.github.com>
2019-01-24 00:47:20 +0100

    Brew formula update for prm version v2.4.1

--- Formula/prm.rb
+++ Formula/prm.rb
@@ -1,9 +1,9 @@
 class Prm < Formula
   desc "Pull Request Manager for Maintainers"
   homepage "https://github.com/ldez/prm"
-  url "https://github.com/ldez/prm/releases/download/v2.4.0/prm_v2.4.0_darwin_amd64.tar.gz"
-  version "2.4.0"
-  sha256 "b1201cf4c7ee351595eae2f669aed0fe0ad0e775bbb6fa8955a08c1cdfddf332"
+  url "https://github.com/ldez/prm/releases/download/v2.4.1/prm_v2.4.1_darwin_amd64.tar.gz"
+  version "2.4.1"
+  sha256 "05c8b28678923abc66a39f330d663ca089740b7306e34d2cbb284184bb9bf6af"
 
   def install
     bin.install "prm"
