3765654842c62ebe67441b49d2e9654ed0be750d
ldez <ldez@users.noreply.github.com>
2019-05-09 23:56:32 +0200

    Brew formula update for seihon version v0.4.2

--- Formula/seihon.rb
+++ Formula/seihon.rb
@@ -2,9 +2,9 @@
 class Seihon < Formula
   desc "A simple tool to publish multi-arch images on the Docker Hub."
   homepage "https://github.com/ldez/seihon"
-  url "https://github.com/ldez/seihon/releases/download/v0.4.1/seihon_v0.4.1_darwin_amd64.tar.gz"
-  version "0.4.1"
-  sha256 "d39797146ee243bed94164896b3cfff3684d6b590a676a09b954a31d4aae4186"
+  url "https://github.com/ldez/seihon/releases/download/v0.4.2/seihon_v0.4.2_darwin_amd64.tar.gz"
+  version "0.4.2"
+  sha256 "7b25a6518b19b90447db27296e2602e4c2fe2c110e7603c08f937f5ad52f3575"
 
   def install
     bin.install "seihon"
