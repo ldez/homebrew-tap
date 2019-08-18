3379e58f7130f82d539159459e13df87fcc9a783
ldez <ldez@users.noreply.github.com>
2019-08-18 03:11:24 +0200

    Brew formula update for gti version v1.5.0

--- Formula/gti.rb
+++ Formula/gti.rb
@@ -2,16 +2,16 @@
 class Gti < Formula
   desc "Just a silly git launcher, basically. Inspired by sl."
   homepage "https://github.com/ldez/gti"
-  version "1.4.0"
+  version "1.5.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ldez/gti/releases/download/v1.4.0/gti_v1.4.0_darwin_amd64.tar.gz"
-    sha256 "71185d57885b33082f6fb218c883e57d0653a9e5373ccf3bfe06a11c99b3bffd"
+    url "https://github.com/ldez/gti/releases/download/v1.5.0/gti_v1.5.0_darwin_amd64.tar.gz"
+    sha256 "7a729081d641ada1c2d00b5e642040b283f574d222dc3fdf4b97fc457016fd1a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ldez/gti/releases/download/v1.4.0/gti_v1.4.0_linux_amd64.tar.gz"
-      sha256 "b87f6e2769cd9d3f913de8511ff8279831a9e086a64fb16bc2b5271f2e61f5ff"
+      url "https://github.com/ldez/gti/releases/download/v1.5.0/gti_v1.5.0_linux_amd64.tar.gz"
+      sha256 "cc2cdad3713ff6efa1504d53036a60d7f7ded82813d877278259c844a594c433"
     end
   end
 
