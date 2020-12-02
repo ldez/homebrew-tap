47292cd811193d23401ba1ae41fa52c95dd555ea
ldez <ldez@users.noreply.github.com>
2020-12-02 23:01:24 +0100

    Brew formula update for gcg version v1.7.4

--- Formula/gcg.rb
+++ Formula/gcg.rb
@@ -2,17 +2,20 @@
 class Gcg < Formula
   desc "GitHub Changelog Generator"
   homepage "https://github.com/ldez/gcg"
-  version "1.7.3"
+  version "1.7.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ldez/gcg/releases/download/v1.7.3/gcg_v1.7.3_darwin_amd64.tar.gz"
-    sha256 "6d0bc0586f298df0a70adeaf96a436d896351dc477828ee61ce4fe2ff392ca4e"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/ldez/gcg/releases/download/v1.7.3/gcg_v1.7.3_linux_amd64.tar.gz"
-      sha256 "1dd27857204dbe7e77a83f3d1b33dc8daee5377ab273c8bd78db3cc2892a3258"
-    end
+    url "https://github.com/ldez/gcg/releases/download/v1.7.4/gcg_v1.7.4_darwin_amd64.tar.gz"
+    sha256 "426547dc281bafb51444a45a3b1d421094604709d0e7576403313e0b529068dd"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/ldez/gcg/releases/download/v1.7.4/gcg_v1.7.4_linux_amd64.tar.gz"
+    sha256 "4f7fb1de2335093ad252aa2f01dff9d543d967bf7e63b930840c96186ba2c809"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/ldez/gcg/releases/download/v1.7.4/gcg_v1.7.4_linux_arm64.tar.gz"
+    sha256 "d5b9d1a440a25776c413ad97d70962ce86bf0753d845464d0a9c6a9a514ce859"
   end
 
   def install
