d6486773715f075fbba28d4779ad6cf2b05b54b4
ldez <ldez@users.noreply.github.com>
2020-12-02 21:37:11 +0100

    Brew formula update for prm version v3.4.3

--- Formula/prm.rb
+++ Formula/prm.rb
@@ -2,23 +2,20 @@
 class Prm < Formula
   desc "Pull Request Manager for Maintainers"
   homepage "https://github.com/ldez/prm"
-  version "3.4.0"
+  version "3.4.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ldez/prm/releases/download/v3.4.0/prm_v3.4.0_darwin_amd64.tar.gz"
-    sha256 "1214f8da1d6b24aec12ee8ee9f619161e25b664d838f08450b6895730dc20c70"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/ldez/prm/releases/download/v3.4.0/prm_v3.4.0_linux_amd64.tar.gz"
-      sha256 "ec3cc513bd7a8d22a6518392bf00e90289981a63500f3a36240749a95dfb7e74"
-    end
-    if Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://github.com/ldez/prm/releases/download/v3.4.0/prm_v3.4.0_linux_arm64.tar.gz"
-        sha256 "a0dff619632331107c6ad6621f1989092a38a546aad85ed7ed926c75d56e3440"
-      end
-    end
+    url "https://github.com/ldez/prm/releases/download/v3.4.3/prm_v3.4.3_darwin_amd64.tar.gz"
+    sha256 "31cdbbeea78629db214eca57b06842019e9bf1a136fc5545a2957534898ef4db"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/ldez/prm/releases/download/v3.4.3/prm_v3.4.3_linux_amd64.tar.gz"
+    sha256 "7c792514bdccdc29b66b9f4036ab7c06ea3a482f8aae17b8bb361ccaf4a69db7"
+  end
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://github.com/ldez/prm/releases/download/v3.4.3/prm_v3.4.3_linux_arm64.tar.gz"
+    sha256 "bfee71c004227c880b51837922a10759e50f1a32034042cbc94226a7c84a9364"
   end
 
   def install
